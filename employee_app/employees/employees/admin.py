09df4b858eaf4ff966c7ace0a5acda2074251b3c
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-02-03 14:11:17 +0200

    Add files via upload

---
+++ employee_app/employees/employees/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from employee_app.employees.models import Employee, Department
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'job_title', 'company')
+    # pass
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass

d6a6a0673e23f9c7f2478ec259393e3ffb91a93d
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-02-03 14:12:41 +0200

    Add files via upload

---
+++ employee_app/templates_examples/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TemplatesExamplesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'employee_app.templates_examples'

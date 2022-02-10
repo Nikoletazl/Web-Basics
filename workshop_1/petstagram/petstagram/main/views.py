db9df741ecc44ea1e4e3a0d0be5fbc279ea3f460
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-02-10 12:15:48 +0200

    Update views.py

--- workshop_1/petstagram/petstagram/main/views.py
+++ workshop_1/petstagram/petstagram/main/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from petstagram.main.models import Profile, PetPhoto
+
+
+def get_profile():
+    profiles = Profile.objects.all()
+    if profiles:
+        return profiles[0]
 
 
 def show_home(request):
-    return render(request, 'home_page.html')
+    context = {
+        'hide_additional_nav_items': True,
+    }
+
+    return render(request, 'home_page.html', context)
 
 
 def show_dashboard(request):
-    return render(request, 'dashboard.html')
+    profile = get_profile()
+    pet_photos = set(PetPhoto.objects.filter(tagged_pets__user_profile=profile))
+    context = {
+        'pet_photos': pet_photos,
+    }
+
+    return render(request, 'dashboard.html', context)
 
 
 def show_profile(request):
@@ -14,4 +32,17 @@ def show_profile(request):
 
 
 def show_pet_photo_details(request, pk):
-    return render(request, 'photo_details.html')
+    pet_photo = PetPhoto.objects.get(pk=pk)
+    context = {
+        'pet_photo': pet_photo,
+    }
+    return render(request, 'photo_details.html', context)
+
+
+def like_pet(request, pk):
+    pet_photo = PetPhoto.objects.get(pk=pk)
+    pet_photo.likes += 1
+    pet_photo.save()
+
+    return redirect('pet photo details', pk)
+

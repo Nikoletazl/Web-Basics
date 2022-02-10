b21c241266f6473ed66d81e54e61ab1fa5c2adf3
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-02-10 12:16:06 +0200

    Update urls.py

--- workshop_1/petstagram/petstagram/main/urls.py
+++ workshop_1/petstagram/petstagram/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from petstagram.main.views import show_home, show_dashboard, show_profile, show_pet_photo_details
+from petstagram.main.views import show_home, show_dashboard, show_profile, show_pet_photo_details, like_pet
 
 urlpatterns = (
     path('', show_home, name='index'),
     path('dashboard/', show_dashboard, name='dashboard'),
     path('profile/', show_profile, name='profile'),
     path('photo/details/<int:pk>/', show_pet_photo_details, name='pet photo details'),
+    path('photo/like/<int:pk>/', like_pet, name='like pet photo'),
 )
